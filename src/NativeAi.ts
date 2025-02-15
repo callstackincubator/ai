32fe0fff6ee9b2414744e43943ddced882d477c6
szymonrybczak <szymon.rybczak@gmail.com>
2025-02-15 13:34:21 +0100

    fix: types

--- src/NativeAi.ts
+++ src/NativeAi.ts
@@ -1,7 +1,6 @@
 import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
-import type { AiModelSettings } from '../lib/typescript/src';
-
+import type { AiModelSettings } from './index';
 export interface Spec extends TurboModule {
   getModel(name: string): Promise<string>; // Returns JSON string of ModelInstance
   getModels(): Promise<AiModelSettings[]>; // Returns array with available models
