6af96d0c301e1c6a2d5a85a283831e0c24eeb2b2
szymonrybczak <szymon.rybczak@gmail.com>
2025-02-17 22:30:15 +0100

    chore: format

--- ios/Ai.mm
+++ ios/Ai.mm
@@ -36,186 +36,7 @@ RCT_EXPORT_MODULE()
   if (self) {
     _engine = [[MLCEngine alloc] init];
 
-        // Locate the config file in the bundle
-        _bundleURL = [[[NSBundle mainBundle] bundleURL] URLByAppendingPathComponent:@"bundle"];
-        NSURL *configURL = [_bundleURL URLByAppendingPathComponent:@"mlc-app-config.json"];
-
-        // Read and parse JSON
-        NSData *jsonData = [NSData dataWithContentsOfURL:configURL];
-        if (jsonData) {
-            NSError *error;
-            NSDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:&error];
-
-            if (!error && [jsonDict isKindOfClass:[NSDictionary class]]) {
-                NSArray *modelList = jsonDict[@"model_list"];
-                if ([modelList isKindOfClass:[NSArray class]] && modelList.count > 0) {
-                    NSDictionary *firstModel = modelList[0];
-                    _modelPath = firstModel[@"model_path"];
-                    _modelLib = firstModel[@"model_lib"];
-                }
-            }
-        }
-    }
-    return self;
-}
-
-- (NSDictionary *)parseResponseString:(NSString *)responseString {
-    NSData *jsonData = [responseString dataUsingEncoding:NSUTF8StringEncoding];
-    NSError *error;
-    NSArray *jsonArray = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:&error];
-
-    if (error) {
-        NSLog(@"Error parsing JSON: %@", error);
-        return nil;
-    }
-
-    if (jsonArray.count > 0) {
-        NSDictionary *responseDict = jsonArray[0];
-        NSArray *choices = responseDict[@"choices"];
-        if (choices.count > 0) {
-            NSDictionary *choice = choices[0];
-            NSDictionary *delta = choice[@"delta"];
-            NSString *content = delta[@"content"];
-            NSString *finishReason = choice[@"finish_reason"];
-
-            BOOL isFinished = (finishReason != nil && ![finishReason isEqual:[NSNull null]]);
-
-            return @{
-                @"content": content ?: @"",
-                @"isFinished": @(isFinished)
-            };
-        }
-    }
-
-    return nil;
-}
-
-RCT_EXPORT_METHOD(doGenerate:(NSString *)instanceId
-                  messages:(NSArray<NSDictionary *> *)messages
-                  resolve:(RCTPromiseResolveBlock)resolve
-                  reject:(RCTPromiseRejectBlock)reject)
-{
-    NSLog(@"Generating for instance ID: %@, with text: %@", instanceId, messages);
-    _displayText = @"";
-    __block BOOL hasResolved = NO;
-
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        NSURL *modelLocalURL = [self.bundleURL URLByAppendingPathComponent:self.modelPath];
-        NSString *modelLocalPath = [modelLocalURL path];
-
-        [self.engine reloadWithModelPath:modelLocalPath modelLib:self.modelLib];
-
-        [self.engine chatCompletionWithMessages:messages completion:^(id response) {
-            if ([response isKindOfClass:[NSString class]]) {
-                NSDictionary *parsedResponse = [self parseResponseString:response];
-                if (parsedResponse) {
-                    NSString *content = parsedResponse[@"content"];
-                    BOOL isFinished = [parsedResponse[@"isFinished"] boolValue];
-
-                    if (content) {
-                        self.displayText = [self.displayText stringByAppendingString:content];
-                    }
-
-                    if (isFinished && !hasResolved) {
-                        hasResolved = YES;
-                        resolve(self.displayText);
-                    }
-
-                } else {
-                    if (!hasResolved) {
-                        hasResolved = YES;
-                        reject(@"PARSE_ERROR", @"Failed to parse response", nil);
-                    }
-                }
-            } else {
-                if (!hasResolved) {
-                    hasResolved = YES;
-                    reject(@"INVALID_RESPONSE", @"Received an invalid response type", nil);
-                }
-            }
-        }];
-    });
-}
-
-RCT_EXPORT_METHOD(doStream:(NSString *)instanceId
-                  text:(NSString *)text
-                  resolve:(RCTPromiseResolveBlock)resolve
-                  reject:(RCTPromiseRejectBlock)reject)
-{
-
-    NSLog(@"Streaming for instance ID: %@, with text: %@", instanceId, text);
-
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        __block BOOL hasResolved = NO;
-
-        NSURL *modelLocalURL = [self.bundleURL URLByAppendingPathComponent:self.modelPath];
-        NSString *modelLocalPath = [modelLocalURL path];
-
-        [self.engine reloadWithModelPath:modelLocalPath modelLib:self.modelLib];
-
-        NSDictionary *message = @{
-            @"role": @"user",
-            @"content": text
-        };
-
-        [self.engine chatCompletionWithMessages:@[message] completion:^(id response) {
-            if ([response isKindOfClass:[NSString class]]) {
-                NSDictionary *parsedResponse = [self parseResponseString:response];
-                if (parsedResponse) {
-                    NSString *content = parsedResponse[@"content"];
-                    BOOL isFinished = [parsedResponse[@"isFinished"] boolValue];
-
-                    if (content) {
-                        self.displayText = [self.displayText stringByAppendingString:content];
-                        if (self->hasListeners) {
-                             [self sendEventWithName:@"onChatUpdate" body:@{@"content": content}];
-                         }
-                    }
-
-                    if (isFinished && !hasResolved) {
-                        hasResolved = YES;
-                        if (self->hasListeners) {
-                             [self sendEventWithName:@"onChatComplete" body:nil];
-                         }
-
-                        resolve(@"");
-
-                        return;
-                    }
-                } else {
-                    if (!hasResolved) {
-                        hasResolved = YES;
-                        reject(@"PARSE_ERROR", @"Failed to parse response", nil);
-                    }
-                }
-            } else {
-                if (!hasResolved) {
-                    hasResolved = YES;
-                    reject(@"INVALID_RESPONSE", @"Received an invalid response type", nil);
-                }
-            }
-        }];
-    });
-}
-
-
-RCT_EXPORT_METHOD(getModel:(NSString *)name
-                  resolve:(RCTPromiseResolveBlock)resolve
-                  reject:(RCTPromiseRejectBlock)reject)
-{
-    NSLog(@"Getting model: %@", name);
-    // TODO: add a logic for fetching models if they're not presented in the `bundle/` directory.
-    NSDictionary *modelInfo = @{
-        @"path": self.modelPath,
-        @"lib": self.modelLib
-    };
-
-    resolve(modelInfo);
-}
-
-RCT_EXPORT_METHOD(getModels:(RCTPromiseResolveBlock)resolve
-                  reject:(RCTPromiseRejectBlock)reject)
-{
+    // Locate the config file in the bundle
     _bundleURL = [[[NSBundle mainBundle] bundleURL] URLByAppendingPathComponent:@"bundle"];
     NSURL* configURL = [_bundleURL URLByAppendingPathComponent:@"mlc-app-config.json"];
 
@@ -266,9 +87,9 @@ RCT_EXPORT_METHOD(getModels:(RCTPromiseResolveBlock)resolve
   return nil;
 }
 
-RCT_EXPORT_METHOD(doGenerate : (NSString*)instanceId text : (NSString*)text resolve : (RCTPromiseResolveBlock)
+RCT_EXPORT_METHOD(doGenerate : (NSString*)instanceId messages : (NSArray<NSDictionary*>*)messages resolve : (RCTPromiseResolveBlock)
                       resolve reject : (RCTPromiseRejectBlock)reject) {
-  NSLog(@"Generating for instance ID: %@, with text: %@", instanceId, text);
+  NSLog(@"Generating for instance ID: %@, with text: %@", instanceId, messages);
   _displayText = @"";
   __block BOOL hasResolved = NO;
 
@@ -278,9 +99,7 @@ RCT_EXPORT_METHOD(doGenerate : (NSString*)instanceId text : (NSString*)text reso
 
     [self.engine reloadWithModelPath:modelLocalPath modelLib:self.modelLib];
 
-    NSDictionary* message = @{@"role" : @"user", @"content" : text};
-
-    [self.engine chatCompletionWithMessages:@[ message ]
+    [self.engine chatCompletionWithMessages:messages
                                  completion:^(id response) {
                                    if ([response isKindOfClass:[NSString class]]) {
                                      NSDictionary* parsedResponse = [self parseResponseString:response];
