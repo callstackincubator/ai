0e8c5b70c982f706eff76a9eeb5bcb897fd039c3
Q1w1N <47570093+Q1w1N@users.noreply.github.com>
2025-02-17 21:29:06 +0100

    chore: android setup with mocks in place (#9)
    * chore: android setup with mocks in place
    
    * chore: lint
    
    * chore: update gh actions
    
    * fix: setup local.properties on CI
    
    ---------
    
    Co-authored-by: szymonrybczak <szymon.rybczak@gmail.com>

--- android/src/main/java/com/ai/AiModule.kt
+++ android/src/main/java/com/ai/AiModule.kt
@@ -1,21 +1,54 @@
 package com.ai
 
-import com.facebook.react.bridge.Promise
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.*
+import com.facebook.react.module.annotations.ReactModule
+import com.facebook.react.turbomodule.core.interfaces.TurboModule
+import org.json.JSONObject
 
-class AiModule internal constructor(context: ReactApplicationContext) : AiSpec(context) {
+@ReactModule(name = AiModule.NAME)
+class AiModule(reactContext: ReactApplicationContext) :
+  ReactContextBaseJavaModule(reactContext),
+  TurboModule {
+  companion object {
+    const val NAME = "Ai"
+  }
 
   override fun getName(): String = NAME
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
   @ReactMethod
-  override fun multiply(a: Double, b: Double, promise: Promise) {
-    promise.resolve(a * b)
+  fun getModel(name: String, promise: Promise) {
+    try {
+      val modelInstance = JSONObject().apply {
+        put("model_id", name)
+      }
+      promise.resolve(modelInstance.toString())
+    } catch (e: Exception) {
+      promise.reject("JSON_ERROR", "Error creating JSON object", e)
+    }
   }
 
-  companion object {
-    const val NAME = "Ai"
+  @ReactMethod
+  fun getModels(promise: Promise) {
+    try {
+      val modelsArray = Arguments.createArray().apply {
+        val model1 = Arguments.createMap().apply { putString("model_id", "mock_model_1") }
+        val model2 = Arguments.createMap().apply { putString("model_id", "mock_model_2") }
+        pushMap(model1)
+        pushMap(model2)
+      }
+      promise.resolve(modelsArray)
+    } catch (e: Exception) {
+      promise.reject("JSON_ERROR", "Error creating JSON array", e)
+    }
+  }
+
+  @ReactMethod
+  fun doGenerate(instanceId: String, text: String, promise: Promise) {
+    promise.resolve("Generated text for $instanceId: $text")
+  }
+
+  @ReactMethod
+  fun doStream(instanceId: String, text: String, promise: Promise) {
+    promise.resolve("Streaming text for $instanceId: $text")
   }
 }
--- scripts/format-ios.sh
+++ scripts/format-ios.sh
@@ -2,5 +2,5 @@
 
 find ios -type f \( -name "*.h" -o -name "*.cpp" -o -name "*.m" -o -name "*.mm" \) -print0 | while read -d $'\0' file; do
   echo "Formatting $file"
-  clang-format -style=file:./ios/.clang-format -i "$file"
+  clang-format -style=file:./ios/.clang-format -i "$file" $1
 done 
