1f517fd95bd0a1920ddd00d2e4872f33efc02fae
Kewin Wereszczynski <kewin.wereszczynski@callstack.com>
2025-02-21 15:29:46 +0100

    feat: integrating with android

--- android/src/main/java/com/ai/AiModule.kt
+++ android/src/main/java/com/ai/AiModule.kt
@@ -4,6 +4,9 @@ import com.facebook.react.bridge.*
 import com.facebook.react.module.annotations.ReactModule
 import com.facebook.react.turbomodule.core.interfaces.TurboModule
 import org.json.JSONObject
+import java.io.File
+import com.google.gson.annotations.SerializedName
+import com.google.gson.Gson
 
 @ReactModule(name = AiModule.NAME)
 class AiModule(reactContext: ReactApplicationContext) :
@@ -11,9 +14,21 @@ class AiModule(reactContext: ReactApplicationContext) :
   TurboModule {
   companion object {
     const val NAME = "Ai"
+
+    const val AppConfigFilename = "mlc-app-config.json"
+    const val ModelConfigFilename = "mlc-chat-config.json"
+    const val ParamsConfigFilename = "ndarray-cache.json"
+    const val ModelUrlSuffix = "resolve/main/"
   }
 
+  private var appConfig = AppConfig(
+    emptyList<String>().toMutableList(),
+    emptyList<ModelRecord>().toMutableList()
+  )
+
   override fun getName(): String = NAME
+
+  private val gson = Gson()
 
   @ReactMethod
   fun getModel(name: String, promise: Promise) {
@@ -30,12 +45,23 @@ class AiModule(reactContext: ReactApplicationContext) :
   @ReactMethod
   fun getModels(promise: Promise) {
     try {
-      val modelsArray = Arguments.createArray().apply {
-        val model1 = Arguments.createMap().apply { putString("model_id", "mock_model_1") }
-        val model2 = Arguments.createMap().apply { putString("model_id", "mock_model_2") }
-        pushMap(model1)
-        pushMap(model2)
+      val appConfigFile = File(reactApplicationContext.applicationContext.getExternalFilesDir(""), AppConfigFilename)
+
+      val jsonString: String = if (appConfigFile.exists()) {
+        appConfigFile.readText()
+      } else {
+        reactApplicationContext.applicationContext.assets.open(AppConfigFilename).bufferedReader().use { it.readText() }
       }
+
+      appConfig = gson.fromJson(jsonString, AppConfig::class.java)
+      appConfig.modelLibs = emptyList<String>().toMutableList()
+
+      val modelsArray = Arguments.createArray().apply {
+        for (modelRecord in appConfig.modelList) {
+          pushMap(Arguments.createMap().apply { putString("model_id", modelRecord.modelId) })
+        }
+      }
+
       promise.resolve(modelsArray)
     } catch (e: Exception) {
       promise.reject("JSON_ERROR", "Error creating JSON array", e)
@@ -52,3 +78,15 @@ class AiModule(reactContext: ReactApplicationContext) :
     promise.resolve("Streaming text for $instanceId: $text")
   }
 }
+
+data class AppConfig(
+  @SerializedName("model_libs") var modelLibs: MutableList<String>,
+  @SerializedName("model_list") val modelList: MutableList<ModelRecord>,
+)
+
+data class ModelRecord(
+  @SerializedName("model_url") val modelUrl: String,
+  @SerializedName("model_id") val modelId: String,
+  @SerializedName("estimated_vram_bytes") val estimatedVramBytes: Long?,
+  @SerializedName("model_lib") val modelLib: String
+)
