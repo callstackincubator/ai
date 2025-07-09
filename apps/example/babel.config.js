44c53f530ca0e41eca5faff53183e881202cb7b6
szymonrybczak <szymon.rybczak@gmail.com>
2025-07-09 20:23:47 +0200

    fix: paths

--- apps/example/babel.config.js
+++ apps/example/babel.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const pak = require('../package.json');
+const pak = require('../../packages/mlc/package.json');
 
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
@@ -10,7 +10,7 @@ module.exports = {
       {
         extensions: ['.tsx', '.ts', '.js', '.json'],
         alias: {
-          [pak.name]: path.join(__dirname, '..', pak.source),
+          [pak.name]: path.join(__dirname, '../../packages/mlc', pak.source),
         },
       },
     ],
