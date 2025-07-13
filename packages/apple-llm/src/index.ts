46f109eade6ecfa5a655a7dc02177931581be547
Mike <grabbou@gmail.com>
2025-07-13 14:47:33 +0400

    feat: structured output (#38)
    * feat: rebase
    
    * fix: ci
    
    * fix: ci 2
    
    * fix: add missing react-native-builder-bob package
    
    * chore: lock
    
    ---------
    
    Co-authored-by: szymonrybczak <szymon.rybczak@gmail.com>

--- packages/apple-llm/src/index.ts
+++ packages/apple-llm/src/index.ts
@@ -5,18 +5,65 @@ import NativeAppleLLMSpec, {
   type AppleMessage,
   type AppleGenerationOptions,
 } from './NativeAppleLLM';
+import z from 'zod';
+import { type LanguageModelV1StreamPart } from '@ai-sdk/provider';
 
 export function apple(): AppleLLMChatLanguageModel {
   return new AppleLLMChatLanguageModel();
 }
 
+interface StructuredGenerationOptions<T> extends AppleGenerationOptions {
+  schema: z.ZodType<T>;
+}
+
+interface GenerationOptions extends AppleGenerationOptions {
+  schema?: undefined;
+}
+
+async function generateText<T>(
+  messages: AppleMessage[],
+  options: StructuredGenerationOptions<T>
+): Promise<T>;
+
+async function generateText(
+  messages: AppleMessage[],
+  options?: GenerationOptions
+): Promise<string>;
+
+async function generateText(
+  messages: AppleMessage[],
+  options: StructuredGenerationOptions<unknown> | GenerationOptions = {}
+): Promise<unknown> {
+  const schema = 'schema' in options ? options.schema : undefined;
+
+  const generationOptions = {
+    ...options,
+    ...(schema instanceof z.ZodType ? { schema: z.toJSONSchema(schema) } : {}),
+  };
+
+  const response = await NativeAppleLLMSpec.generateText(
+    messages,
+    generationOptions
+  );
+
+  if (schema instanceof z.ZodType) {
+    const parsed = schema.parse(JSON.parse(response));
+    return parsed;
+  }
+
+  return response;
+}
+
 export const foundationModels = {
   isAvailable: NativeAppleLLMSpec.isAvailable,
-  generateText(
+  generateText,
+  generateStream(
     messages: AppleMessage[],
     options: AppleGenerationOptions = {}
-  ): Promise<string> {
-    return NativeAppleLLMSpec.generateText(messages, options);
+  ): ReadableStream<LanguageModelV1StreamPart> {
+    if (options.schema instanceof z.ZodObject) {
+      options.schema = z.toJSONSchema(options.schema);
+    }
+    return generateStream(messages, options);
   },
-  generateStream,
 };
