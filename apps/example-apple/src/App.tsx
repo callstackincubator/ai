a3252286602bff3843be20365bf92fb8e819aac1
Mike Grabowski <grabbou@gmail.com>
2025-08-26 23:59:35 +0200

    feat: bottom tabs

--- apps/example-apple/src/App.tsx
+++ apps/example-apple/src/App.tsx
@@ -1,6 +1,6 @@
 import './global.css'
 
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createNativeBottomTabNavigator } from '@bottom-tabs/react-navigation'
 import { NavigationContainer } from '@react-navigation/native'
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
@@ -10,16 +10,40 @@ import PlaygroundScreen from './screens/PlaygroundScreen'
 import SpeechScreen from './screens/SpeechScreen'
 import TranscribeScreen from './screens/TranscribeScreen'
 
-const Tab = createBottomTabNavigator()
+const Tab = createNativeBottomTabNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="LLM" component={LLMScreen} />
-        <Tab.Screen name="Playground" component={PlaygroundScreen} />
-        <Tab.Screen name="Transcribe" component={TranscribeScreen} />
-        <Tab.Screen name="Speech" component={SpeechScreen} />
+        <Tab.Screen 
+          name="LLM" 
+          component={LLMScreen}
+          options={{
+            tabBarIcon: () => ({ sfSymbol: 'brain.head.profile' }),
+          }}
+        />
+        <Tab.Screen 
+          name="Playground" 
+          component={PlaygroundScreen}
+          options={{
+            tabBarIcon: () => ({ sfSymbol: 'play.circle' }),
+          }}
+        />
+        <Tab.Screen 
+          name="Transcribe" 
+          component={TranscribeScreen}
+          options={{
+            tabBarIcon: () => ({ sfSymbol: 'text.quote' }),
+          }}
+        />
+        <Tab.Screen 
+          name="Speech" 
+          component={SpeechScreen}
+          options={{
+            tabBarIcon: () => ({ sfSymbol: 'speaker.wave.3' }),
+          }}
+        />
       </Tab.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
