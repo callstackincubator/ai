dcc742c776428d7c98cd7fe973d45517cca89a31
Mike Grabowski <grabbou@gmail.com>
2025-09-07 01:33:14 +0200

    chore: align versions to match

--- apps/example-apple/src/App.tsx
+++ apps/example-apple/src/App.tsx
@@ -10,7 +10,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { StatusBar } from 'expo-status-bar'
 import { Provider as JotaiProvider } from 'jotai'
 import React from 'react'
-import { TouchableNativeFeedback } from 'react-native'
+import { Pressable } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { KeyboardProvider } from 'react-native-keyboard-controller'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
@@ -31,23 +31,25 @@ const Stack = createNativeStackNavigator()
 
 function ChatStack() {
   return (
-    <Stack.Navigator
-      screenOptions={({ navigation }) => ({
-        headerLeft: () => {
-          return (
-            <TouchableNativeFeedback
-              onPress={() =>
-                // @ts-expect-error
-                navigation.toggleDrawer()
-              }
-            >
-              <Ionicons name="menu" size={24} color="black" />
-            </TouchableNativeFeedback>
-          )
-        },
-      })}
-    >
-      <Stack.Screen name="Chat" component={LLMScreen} />
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Chat"
+        component={LLMScreen}
+        options={({ navigation }) => ({
+          headerLeft: () => {
+            return (
+              <Pressable
+                onPress={() =>
+                  // @ts-ignore
+                  navigation.toggleDrawer()
+                }
+              >
+                <Ionicons name="menu" size={24} color="black" />
+              </Pressable>
+            )
+          },
+        })}
+      />
     </Stack.Navigator>
   )
 }
