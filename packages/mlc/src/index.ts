3e62c698131c1ed227c53b625f00b29c068770a0
Mike Grabowski <grabbou@gmail.com>
2025-09-10 01:07:48 +0200

    chore

--- packages/mlc/src/index.ts
+++ packages/mlc/src/index.ts
@@ -1,3 +1,2 @@
 export { createMlcProvider, mlc } from './ai-sdk'
-export { default as MLCEngine, downloadModel } from './NativeMLCEngine'
-export type { Message, Model, DownloadProgress } from './NativeMLCEngine'
+export { default as MLCEngine } from './NativeMLCEngine'
