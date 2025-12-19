fb6adae38d88bf2206b78ccbefa1c50eeb553202
artus9033 <artus9033@gmail.com>
2025-12-19 15:43:03 +0100

    fix: linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import globals from 'globals'
 
 export default defineConfig([
-  globalIgnores(['website/**/*', '**/node_modules', '**/lib']),
+  globalIgnores(['website/**/*', '**/node_modules', '**/lib', '**/build']),
   expoConfig,
   eslintPluginPrettierRecommended,
   {
